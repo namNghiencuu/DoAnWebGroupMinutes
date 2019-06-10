c335957b29cd487486b31d277520c5332ef521d6
NguyenCuong <nguyenhuycuong06@gmail.com>
2019-06-10 20:42:54 +0700

    EditPlayer

--- public/script/signin.js
+++ public/script/signin.js
@@ -29,3 +29,18 @@ $(document).ready(function() {
     }
   });
 });
+var objPeople = [
+  {
+    username = "cuong",
+    password = "cuong11"
+  }
+]
+function getinfo(){
+  var username = document.getElementById("username").value
+  var password = document.getElementById("password").value
+  for(i=0 ; i < objPeople.length ; i++){
+    if(username==objPeople[i].username&&password==objPeople[i].password){
+      console.log(username+" is logged in ")
+    }
+  }
+}
