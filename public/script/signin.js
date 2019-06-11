f18b92aed6e6b65bdb5cbdf5ce131793c9739c7f
NguyenCuong <nguyenhuycuong06@gmail.com>
2019-06-11 15:04:51 +0700

    edit player again

--- public/script/signin.js
+++ public/script/signin.js
@@ -29,18 +29,3 @@ $(document).ready(function() {
     }
   });
 });
-var objPeople = [
-  {
-    username = "cuong",
-    password = "cuong11"
-  }
-]
-function getinfo(){
-  var username = document.getElementById("username").value
-  var password = document.getElementById("password").value
-  for(i=0 ; i < objPeople.length ; i++){
-    if(username==objPeople[i].username&&password==objPeople[i].password){
-      console.log(username+" is logged in ")
-    }
-  }
-}
